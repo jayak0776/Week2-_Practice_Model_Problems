0c512cf72605af63ba4ae29b89c59ea4ec5c1825
Gubbala Jayakumar <115056837+jayak0776@users.noreply.github.com>
2024-06-01 11:20:53 +0530

    listComprehensions.py

--- Week2/list.py
+++ Week2/list.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     n = int(input())
     li=[]
     li=[]
-    for i in range(0,x):
-        for j in range(0,y):
-            for k in range(0,z):
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
                 if ((i+j+k)!=n):
                     li.append([i,j,k])
     print(li)  
